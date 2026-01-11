4b5f47d4ba1318fe55efd43b816920844f9af50c
duarteoliveira <du84arte@gmail.com>
2026-01-11 20:27:03 -0300

    mini-projeto cpf_validator

--- cpf_validator/internal/cpf/model.go
+++ cpf_validator/internal/cpf/model.go
@@ -3,5 +3,5 @@ package cpf
 // cpf representa um cpf valido no sistema/ cpf represents a valid cpf in the system
 // criamos um tipo próprio para dar semanica / we create our own type to give semantics
 // e evitar o uso solto de string. / and avoid the loose use of string.
-type cpf string
+type CPF string
 
