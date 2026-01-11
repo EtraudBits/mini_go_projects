2c00b4b86b9df3ab116ff92f0ad1af38bc5b57f9
duarteoliveira <du84arte@gmail.com>
2026-01-11 19:09:50 -0300

    feat: add tipo cpf com value object

---
+++ cpf_validator/internal/cpf/model.go
@@ -0,0 +1,7 @@
+package cpf
+
+// cpf representa um cpf valido no sistema/ cpf represents a valid cpf in the system
+// criamos um tipo próprio para dar semanica / we create our own type to give semantics
+// e evitar o uso solto de string. / and avoid the loose use of string.
+type cpf string
+
