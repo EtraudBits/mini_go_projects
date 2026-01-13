b938f99e08368277e6de2ebe421c1e68c8fe33ec
duarteoliveira <du84arte@gmail.com>
2026-01-13 20:05:11 -0300

    feat: add erros de dominio para validação de email

---
+++ validador_de_email/internal/email/errors.go
@@ -0,0 +1,25 @@
+package email
+
+import "errors"
+
+var (
+	ErrEmptyEmail = errors.New("email não pode ser vazio") // "email cannot be empty"
+	ErrIvalidAtSymbol = errors.New("email deve conter apenas um @") // "email must contain only one @")
+	ErrInvalidLocalPart = errors.New("email deve conter texto antes do @") // "email must contain text before the @")
+	ErrIvalidDomain = errors.New("email deve conter domínio válido") // "email must contain a valid domain"
+	ErrIvalidDotDomain = errors.New("domínio deve conter pelo menos um ponto") // "domain must contain at least one dot"
+)
+
+/*Um email será considerado válido se:
+
+1 - Não estiver vazio
+
+2 - Contiver exatamente um @
+
+3 - Tiver texto antes do @
+
+4 - Tiver domínio após o @
+
+5 - Domínio contiver pelo menos um ponto (.)
+
+Não começar nem terminar com @ ou .*/
