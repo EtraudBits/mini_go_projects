a2468627b4c83e504fa4b7800fc52d1ab3ad7f89
duarteoliveira <du84arte@gmail.com>
2026-01-29 20:49:53 -0300

    refactor: refatoração do sistema de controle de estoque

--- controleEstoque/main.go
+++ controleEstoque/main.go
@@ -9,29 +9,38 @@ type Produto struct {
 
 }
 
+func cadastrarProduto(nome string, quantidade int) Produto {
+	return Produto{
+		Nome: nome,
+		Quantidade: quantidade, 
+	}
+}
+
+func adicionarEstoque(estoque []Produto, produto Produto) []Produto {
+	estoque  = append(estoque, produto)
+	return estoque
+}
+
+func listarEstoque(estoque []Produto) {
+	for _, produto := range estoque {
+		fmt.Printf("Produto: %s | Quantidade: %d\n", produto.Nome, produto.Quantidade)
+	}
+}
+
 func main() {
 
 	estoque := []Produto{}
 
-		estoque = append(estoque, Produto{
-			Nome: "Cobogo Arabe",
-			Quantidade: 10,
-		})
-		estoque = append(estoque, Produto{
-			Nome: "Cobogo Taco Chines",
-			Quantidade: 2,
-		})
-		estoque = append(estoque, Produto{
-			Nome: "Cobogo Flor",
-			Quantidade: 5,
-		})
-		estoque = append(estoque, Produto{
-			Nome: "Cobogo 4 Pontas",
-			Quantidade: 7,
-		})
+		viga := cadastrarProduto("viga", 11)
+		coluna := cadastrarProduto("coluna", 10)
+		estacaTipoMourao := cadastrarProduto("estaca tipo mourao", 100)
+		estacaCurvada := cadastrarProduto("estaca curvada", 15)
 
-		for i, produto := range estoque {
-			fmt.Printf ("%d - Produto: %s | Quantidade: %d\n", i, produto.Nome, produto.Quantidade)
-		}
-	}
+		estoque = adicionarEstoque(estoque, viga)
+		estoque = adicionarEstoque(estoque, coluna)
+		estoque = adicionarEstoque(estoque, estacaTipoMourao)
+		estoque = adicionarEstoque(estoque, estacaCurvada)
+		listarEstoque(estoque)
+
+}
 
