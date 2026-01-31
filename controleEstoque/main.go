cdb42d48bbf813b33734e877f52eca40287d0cec
duarteoliveira <du84arte@gmail.com>
2026-01-30 21:47:22 -0300

    feat: implemetação de metodos para manipulação de quantidade e exibição de produto

--- controleEstoque/main.go
+++ controleEstoque/main.go
@@ -9,6 +9,23 @@ type Produto struct {
 	Quantidade int
 
 }
+
+// Métodos para aumentar e diminuir a quantidade do produto
+func (p *Produto) AumentarQuantidade(valor int) { 
+	p.Quantidade += valor
+}
+
+// Método para diminuir a quantidade do produto
+func (p *Produto) DiminiurQuantidade(valor int) {
+	if p.Quantidade >= valor { // Verifica se há quantidade suficiente
+		p.Quantidade -= valor // Diminui a quantidade
+	}
+}
+
+func (p *Produto) Exibir() {
+	fmt.Printf("Produto: %s | Quantidade: %d\n", p.Nome, p.Quantidade)
+}
+
 // Função para cadastrar um novo produto
 func cadastrarProduto(nome string, quantidade int) Produto {
 	return Produto{
@@ -18,8 +35,8 @@ func cadastrarProduto(nome string, quantidade int) Produto {
 }
 // Função para adicionar um produto ao estoque
 // Usamos um ponteiro para modificar o slice original
-func adicionarEstoque(estoque *[]Produto, produto Produto) {
-	*estoque  = append(*estoque, produto)
+func adicionarEstoque(estoque *[]Produto, produto Produto) { // recebe o endereço do slice ([]Produto)
+	*estoque  = append(*estoque, produto) // altere o valor dentro desse endereço 
 	
 }
 // Função para listar todos os produtos no estoque	
@@ -37,12 +54,18 @@ func main() {
 		coluna := cadastrarProduto("coluna", 10)
 		estacaTipoMourao := cadastrarProduto("estaca tipo mourao", 100)
 		estacaCurvada := cadastrarProduto("estaca curvada", 15)
-		
+
+		//
+		viga.AumentarQuantidade(6) // Aumenta a quantidade da viga em 6
+		coluna.DiminiurQuantidade(2) // Diminui a quantidade da coluna em 2
+
 		// Adicionando produtos ao estoque chamando a função e passando o endereço do slice
-		adicionarEstoque(&estoque, viga)
+		adicionarEstoque(&estoque, viga) // &estoque passa o endereço do slice
 		adicionarEstoque(&estoque, coluna)
 		adicionarEstoque(&estoque, estacaTipoMourao)
 		adicionarEstoque(&estoque, estacaCurvada)
+
+	// Listando os produtos no estoque
 		listarEstoque(estoque)
 
 }
