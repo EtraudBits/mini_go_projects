d36a488c0fb384185912f7d9083db8faa4ede177
duarteoliveira <du84arte@gmail.com>
2026-02-05 21:17:01 -0300

    feat: implementar RepositorioMemoria com armazenamento em memória

--- controleEstoque/estoque/memoria.go
+++ controleEstoque/estoque/memoria.go
@@ -1,24 +1,38 @@
 package estoque
 
-// EstoqueMemoria implementa o RepositorioEstoque armazenando produtos em memória
-type EstoqueMemoria struct {
+import "errors"
+
+// RepositorioMemoria implementa o RepositorioEstoque armazenando produtos em memória
+type RepositorioMemoria struct {
 	produtos []Produto
 }
 
-// NovoEstoqueMemoria cria um repositório de estoque em memória
-func NovoEstoqueMemoria() *EstoqueMemoria {
-	return &EstoqueMemoria {
+// NovoRepositorioMemoria cria um repositório de estoque em memória
+func NovoRepositorioMemoria() *RepositorioMemoria {
+	return &RepositorioMemoria {
 		produtos: []Produto{},
 	}
 }
 
 // Adicionar insere um produto no estoque em memória.
-func (e *EstoqueMemoria) Adicionar(produto Produto) {
+func (e *RepositorioMemoria) Adicionar(produto Produto) {
 	e.produtos = append(e.produtos, produto)	
 }
 
-// Lista devolve todos os produtos armazenados no estoque em memória.
-func (e *EstoqueMemoria) Listar() []Produto {
+// Atualizar modifica um produto existente no estoque em memória.
+func (r *RepositorioMemoria) Atualizar(produto Produto) error { // percorre a lista de produtos para encontrar o produto com o ID correspondente
+	for i:= range r.produtos { // itera sobre os produtos do repositório
+		if r.produtos[i].ID == produto.ID { // se encontrar o produto com o ID correspondente
+			// atualiza o produto no repositório
+			r.produtos[i] = produto
+			return nil // retorna nil se a atualização for bem-sucedida
+		}
+	}
+	return errors.New("produto não encontrado") // retorna erro se o produto não for encontrado
+}
+// Listar devolve todos os produtos armazenados no estoque em memória.
+func (e *RepositorioMemoria) Listar() []Produto {
 	return e.produtos
 }
+
 
